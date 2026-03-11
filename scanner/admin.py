121b4e04bed40000b5228b4dcf55574635820c21
tkstanch <117085557+tkstanch@users.noreply.github.com>
2026-03-11 17:36:39 +0200

    Update admin.py
    Signed-off-by: tkstanch <117085557+tkstanch@users.noreply.github.com>

--- scanner/admin.py
+++ scanner/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from scanner.models import ScanTarget, Scan, Vulnerability, ExploitMedia
 
+
 class ExploitMediaInline(admin.TabularInline):
     """Inline admin for ExploitMedia to show in Vulnerability admin."""
     model = ExploitMedia
